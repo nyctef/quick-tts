4f46b5cd74456b1c202a53b2a8448fb90ec11eb3
nyctef <nyctef@nyctef.com>
2022-07-25 12:10:23 +0100

    add more commands to list voices and speak custom strings

--- Program.cs
+++ Program.cs
@@ -5,25 +5,49 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        if (!OperatingSystem.IsWindows())
+        {
+            throw new NotSupportedException();
+        }
+
         var rootCommand = new RootCommand();
 
         var testCommand = new Command("test", "read out a test message");
         testCommand.SetHandler(() =>
         {
-            if (!OperatingSystem.IsWindows())
-            {
-                throw new NotSupportedException();
-            }
-
             var synth = new SpeechSynthesizer();
 
-            // Configure the audio output.   
             synth.SetOutputToDefaultAudioDevice();
 
-            // Speak a string.  
             synth.Speak("This example demonstrates a basic use of Speech Synthesizer");
         });
         rootCommand.AddCommand(testCommand);
+
+        var listCommand = new Command("list", "list out available voices");
+        listCommand.SetHandler(() =>
+        {
+            var voices = new SpeechSynthesizer().GetInstalledVoices().Where(x => x.Enabled);
+            Console.WriteLine("Available voices:");
+            foreach (var voice in voices)
+            {
+                Console.WriteLine($"  {voice.VoiceInfo.Name}: {voice.VoiceInfo.Description}");
+            }
+        });
+        rootCommand.AddCommand(listCommand);
+
+        var sayCommand = new Command("say", "send some TTS to the default speaker");
+        var textArgument = new Argument<string>("text");
+        sayCommand.AddArgument(textArgument);
+        sayCommand.SetHandler((string text) =>
+        {
+            var synth = new SpeechSynthesizer();
+            synth.SelectVoice("Microsoft Haruka Desktop");
+
+            synth.SetOutputToDefaultAudioDevice();
+
+            synth.Speak(text);
+        }, textArgument);
+        rootCommand.AddCommand(sayCommand);
 
         rootCommand.Invoke(args);
     }

2ce0ffb373ba4bbe16801cd646cf6e62c114a220
nyctef <nyctef@nyctef.com>
2022-07-25 11:39:20 +0100

    console app to test out system.speech

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.CommandLine;
+using System.Speech.Synthesis;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var rootCommand = new RootCommand();
+
+        var testCommand = new Command("test", "read out a test message");
+        testCommand.SetHandler(() =>
+        {
+            if (!OperatingSystem.IsWindows())
+            {
+                throw new NotSupportedException();
+            }
+
+            var synth = new SpeechSynthesizer();
+
+            // Configure the audio output.   
+            synth.SetOutputToDefaultAudioDevice();
+
+            // Speak a string.  
+            synth.Speak("This example demonstrates a basic use of Speech Synthesizer");
+        });
+        rootCommand.AddCommand(testCommand);
+
+        rootCommand.Invoke(args);
+    }
+}
